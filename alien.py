0cf6f13991bbd8261aeac081272e0c10ead1843a
ispWW <laskov.kirill35@gmail.com>
2025-05-19 11:41:36 +0300

    final release

---
+++ alien.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    def __init__(self, ai_settings, screen):
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Возвращает True, если пришелец достиг края экрана."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Перемещает пришельца влево или вправо."""
+        self.x += self.ai_settings.alien_speed * self.ai_settings.fleet_direction
+        self.rect.x = self.x
